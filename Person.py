d657696665136fce5b0136702becfc7b479a7f85
DIR\zamzam.badruzaman <zam2badruzaman@gmail.com>
2018-08-11 15:57:07 +0700

    init

---
+++ Person.py
@@ -0,0 +1,28 @@
+class Person:
+    num_of_person = 0
+
+    def __init__(self, fname, lname, address):
+        self.fnamae = fname
+        self.lname = lname
+        self.address = address
+        self.email = fname + '.' + lname + '@company.com'
+        Person.num_of_person += 1
+
+    def get_fullname(self):
+        return '{} {}'.format(self.fnamae, self.lname)
+
+    @classmethod
+    def set_person_name(cls, num_of_person):
+        cls.num_of_person = num_of_person
+
+
+class Employee(Person):
+    pass
+
+
+Employee.set_person_name(100)
+
+p1 = Employee('Azam', 'Kasep', 'Kota Garut')
+p2 = Employee('Nadiya', 'Geulis', 'Kota Bogor')
+
+print(Employee.num_of_person)
